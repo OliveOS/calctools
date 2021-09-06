1007b505cb397e747dfa7d002bf7f62981555315
oliverwooding <oliverwooding@icloud.com>
2021-09-06 17:45:29 +0200

    updated Calc2D

--- Calc2D.py
+++ Calc2D.py
@@ -2,11 +2,17 @@
 
 import sympy as sp
 from enum import Enum
+from typing import Tuple
 
+#an enum to select solid type for solids of revolution
 class SolidType(Enum):
     CIRCLE = 0
     SQUARE = 1
     ISOTRI = 2 #isosceles triangle, or 1/2 of a square
+
+#a set of type definitions for compressing and decompressing power functions
+constantSeries = list[int]
+partiallyCompressedPowerFunction = Tuple[sp.core.expr.Expr, constantSeries]
 
 #return a taylor series expansion to an arbitrary number of terms
 def getTaylorSeriesExpansion(func: sp.core.expr.Expr, terms: int) -> sp.core.expr.Expr:
@@ -16,6 +22,15 @@ def getTaylorSeriesExpansion(func: sp.core.expr.Expr, terms: int) -> sp.core.exp
 def singleFunctionVolOfRev(func: sp.core.expr.Expr, solidtype: SolidType, a: int, b: int) -> sp.Float :
     pass
 
+#takes a single power function and compresses it into a series of constants
+def compressPowerFunction(func: sp.core.expr.Expr) -> constantSeries:
+    pass
+
+#takes a constant series and decompresses it into a power function
+def decompressConstantSeries(series: constantSeries) -> sp.core.expr.Expr:
+    pass
+
 #TODO: Write the taylor series function
 #TODO: Write the singleFunctionVolOfRev function
 #TODO: Begin a multivariable calc file
+#TODO: Write the compression/decompression pair
